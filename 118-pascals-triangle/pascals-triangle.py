7135e0a3174ddf801822a07ac2e49565b8ab43f9
Tanvi Ranganekar <72243666+tanvi2020@users.noreply.github.com>
2025-07-18 12:13:40 +0100

    Time: 3 ms (10.22%) | Memory: 17.9 MB (37.04%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,18 +1,20 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-
         if numRows==0:
             return []
         if numRows==1:
             return [[1]]
 
-        prevRows=self.generate(numRows-1)
+        # Else claculate the previous rows using recurssion
+        prevRow=self.generate(numRows-1)
+        
+        # Build a new row
         newRow=[1]*numRows
 
+        # Fill the ith middle values of new row with sum of prevRow [i]th element + prevRow [i-1]th element
         for i in range(1,numRows-1):
-            newRow[i]=prevRows[-1][i-1]+ prevRows [-1][i]
-
-
-        prevRows.append(newRow)
-        return prevRows
-        
+            newRow[i]=prevRow[-1][i]+prevRow[-1][i-1]
+            
+        # Append the new row into the prevRow list and return the PrevRow list as answer
+        prevRow.append(newRow)
+        return prevRow
