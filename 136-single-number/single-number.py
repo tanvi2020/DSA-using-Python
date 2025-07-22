7017a169b5707c17e04b7d1a4ed30e8995079422
Tanvi Ranganekar <72243666+tanvi2020@users.noreply.github.com>
2025-07-22 07:55:32 +0100

    Time: 7 ms (33.41%) | Memory: 19.7 MB (21.98%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,7 +1,4 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        single=nums[0]
-        for i in range(1,len(nums)):
-            single=single^nums[i]
-        return single
+        return functools.reduce(lambda i,j:i^j,nums)
         
